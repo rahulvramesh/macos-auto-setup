792a10ef22501063683bce2416efaa1afcc86153
Rahul V Ramesh <rahulvramesh@hotmail.com>
2019-03-13 16:41:03 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,3 +27,59 @@ brew tap caskroom/cask
 brew tap homebrew/cask-versions
 brew tap homebrew/cask-drivers
 
+echo "======================================"
+echo "Install git"
+echo "======================================"
+brew install git
+
+echo "======================================"
+echo "set git email and name"
+echo "What is your git name?"
+#read gitname
+echo "What is your git email?"
+#read gitemail
+echo "======================================"
+git config --global user.name rahulvramesh
+git config --global user.email rahulvramesh@gmail.com
+
+echo "======================================"
+echo "set git color output"
+echo "======================================"
+git config --global color.ui auto
+
+echo "======================================"
+echo "custom git log"
+echo "======================================"
+git config --global format.pretty format:"%C(auto)%h %d%Creset %s%n%Cgreen%ad%Creset %aN <%aE>%n"
+
+echo "======================================"
+echo "set vscode as default git editor"
+echo "======================================"
+git config --global core.editor "code --wait"
+
+echo "======================================"
+echo "Install postman"
+echo "======================================"
+brew cask install postman
+
+echo "======================================"
+echo "Install docker"
+echo "======================================"
+brew cask install docker
+
+echo "======================================"
+echo "Install vscode"
+echo "======================================"
+brew cask install visual-studio-code
+
+echo "======================================"
+echo "Install vlc"
+echo "======================================"
+brew cask install vlc
+
+echo "======================================"
+echo "Install slack"
+echo "======================================"
+brew cask install slack
+
+
